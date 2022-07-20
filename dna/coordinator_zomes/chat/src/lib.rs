9c90f8499680c68c474c67916b770ba9cc2e9418
Matthias <tuttiacc@hotmail.com>
2022-07-20 15:33:09 +0200

    added pseudo code structure

--- dna/coordinator_zomes/chat/src/lib.rs
+++ dna/coordinator_zomes/chat/src/lib.rs
@@ -7,7 +7,12 @@ use chat_integrity::*;
 
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct GroupSecret(String);
+pub struct GroupInfo{
+  secret: String,
+  hashed_secret: Vec<u8>,
+};
+
+
 
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -16,31 +21,38 @@ pub struct Message(char);
 
 
 #[hdk_extern]
-pub fn create_group(secret: GroupSecret) -> ExternResult<ActionHash> {
-  let hashed_secret = hash(secret);
+pub fn join_group(secret: String) -> ExternResult<ActionHash> {
 
+  let hashed_secret = hash_sha256(secret.into_vec());
 
-  let action_hash = create_link(hashed_secret, LinkTypes::GroupSecretToAgent)?;
+  // create HoloHash from hashed_secret
+
+  // get all existinglinks
+  // create two links Me <-> Secret & Secret <-> Me
+  // if there are agents already linked to this Secret --> send signal
+
+  let action_hash = create_link(hashed_secret, agent_pub_key,  LinkTypes::GroupSecretToAgent, secret.into())?;
   unimplemented!()
+
 }
+
 
 
 #[hdk_extern]
-pub fn join_group(secret: GroupSecret) -> ExternResult<ActionHash> {
+pub fn get_my_groups(_: ()) -> ExternResult<Vec<GroupInfo>> {
   unimplemented!()
+  // get all links from my pubkey to GroupSecret
+  // add link tags to vector and return
+
+
+
 }
 
 
-#[hdk_extern]
-pub fn get_my_groups(_: ()) -> ExternResult<Vec<GroupSecret>> {
-  unimplemented!()
-}
-
-
-#[hdk_extern]
-pub fn leave_group(_: ()) -> ExternResult<ActionHash> {
-  unimplemented!()
-}
+// #[hdk_extern]
+// pub fn leave_group(_: ()) -> ExternResult<ActionHash> {
+//   unimplemented!()
+// }
 
 #[hdk_extern]
 pub fn get_group_members(_: GroupSecret) -> ExternResult<Vec<AgentPubKey>> {
@@ -51,6 +63,10 @@ pub fn get_group_members(_: GroupSecret) -> ExternResult<Vec<AgentPubKey>> {
 #[hdk_extern]
 pub fn send_letter(char: Message) -> ExternResult<()> {
   unimplemented!()
+  // {
+  //   letter: char,
+  //   timestamp: i32
+  // }
   // send remote_signal
 }
 
@@ -58,7 +74,7 @@ pub fn send_letter(char: Message) -> ExternResult<()> {
 
 #[hdk_extern]
 fn recv_remote_signal(signal: SerializedBytes) -> ExternResult<()> {
-
+    // decode and emit to the UI
     emit_signal(&signal)?;
     Ok(())
 }
