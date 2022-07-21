5ba7deb14c8fc5f5497511d859e3e9f86cfd40ca
Matthias <tuttiacc@hotmail.com>
2022-07-21 15:52:52 +0200

    updated backend data structure

--- ui/src/components/chat-screen.ts
+++ ui/src/components/chat-screen.ts
@@ -58,14 +58,14 @@ export class ChatScreen extends LitElement {
     console.log("this.service");
     console.log(this.service);
   }
-  
+
   receiveEmojiCannonSignal(signal: AppSignal) {
     // @TODO: filter by agentPubKey, check if agent exist as chat-bubble
   }
-  
+
   receiveMessageSignal(signal: AppSignal) {
     // @TODO: filter by agentPubKey, check if agent exist as chat-bubble
-  }  
+  }
 
   receiveJoinSignal(signal: AppSignal) {
     const joiningMember = signal.data.payload.agent;
@@ -78,67 +78,81 @@ export class ChatScreen extends LitElement {
     // @TODO => ensure that joining member is of type AgentPubKeyB64
     // this.channelMembers = [...this.channelMembers, joiningMember];
   }
-      
-  render() {
-    // const chatBubbles: any[] = [
-    //   {
-    //     channel: this.channel,
-    //     username: "dcts",
-    //     avatarUrl: "https://img.seadn.io/files/66196dd65af5e25c2fac209b0e33bd8d.png?auto=format&fit=max&w=256",
-    //     agentPubKey: "ascou3v8asv8yx0984v0p7duzk"
-    //   },
-    //   {
-    //     channel: this.channel,
-    //     username: "Art Brock",
-    //     avatarUrl: "https://img.seadn.io/files/45e5b8384841b475e7411dafd6c6291a.png?auto=format&fit=max&w=256",
-    //     agentPubKey: "x7f33168savLSKJOIQzasd"
-    //   },
-    //   {
-    //     channel: this.channel,
-    //     username: "dcts",
-    //     avatarUrl: "https://img.seadn.io/files/4f809b585367ec71fa19daba04066cd0.png?auto=format&fit=max&w=256",
-    //     agentPubKey: "v8274sduv2874eva98dv0lki"
-    //   },
-    // ]
+
+
+  renderChannelSelector() {
     return html`
-      <h1>${this.channel}</h1>
-      <!-- ${this.channelMembers.map(channelMember => {
-        let avatar = randomAvatar();
-        let { username, agentPubKey } = chatBubbleObj;
-        return html`<chat-bubble
-          .channel=${this.channel}
-          .username=${username}
-          .avatarUrl=${avatar}
-          .agentPubKey=${agentPubKey}
-        >${username}</chat-bubble>`
-      })} -->
+      <div style="display: flex; flex-direction: column; align-items: center;">
+        <div style="font">Current Channel</div>
+        <input .value=${this.channel!} style="all: unset; border-bottom: 2px solid black;"/>
+      </div>
     `
-    // if (!this._entryDef0) {
-    //   return html`<div style="display: flex; flex: 1; align-items: center; justify-content: center">
-    //     <mwc-circular-progress indeterminate></mwc-circular-progress>
-    //   </div>`;
-    // }
+  }
 
+  render() {
+
+    return html`
+     ${this.renderChannelSelector()}
+    `;
+    // // const chatBubbles: any[] = [
+    // //   {
+    // //     channel: this.channel,
+    // //     username: "dcts",
+    // //     avatarUrl: "https://img.seadn.io/files/66196dd65af5e25c2fac209b0e33bd8d.png?auto=format&fit=max&w=256",
+    // //     agentPubKey: "ascou3v8asv8yx0984v0p7duzk"
+    // //   },
+    // //   {
+    // //     channel: this.channel,
+    // //     username: "Art Brock",
+    // //     avatarUrl: "https://img.seadn.io/files/45e5b8384841b475e7411dafd6c6291a.png?auto=format&fit=max&w=256",
+    // //     agentPubKey: "x7f33168savLSKJOIQzasd"
+    // //   },
+    // //   {
+    // //     channel: this.channel,
+    // //     username: "dcts",
+    // //     avatarUrl: "https://img.seadn.io/files/4f809b585367ec71fa19daba04066cd0.png?auto=format&fit=max&w=256",
+    // //     agentPubKey: "v8274sduv2874eva98dv0lki"
+    // //   },
+    // // ]
     // return html`
-    //   <div style="display: flex; flex-direction: column">
-    //     <span style="font-size: 18px">EntryDef0</span>
+    //   <h1>${this.channel}</h1>
+    //   <!-- ${this.channelMembers.map(channelMember => {
+    //     let avatar = randomAvatar();
+    //     let { username, agentPubKey } = chatBubbleObj;
+    //     return html`<chat-bubble
+    //       .channel=${this.channel}
+    //       .username=${username}
+    //       .avatarUrl=${avatar}
+    //       .agentPubKey=${agentPubKey}
+    //     >${username}</chat-bubble>`
+    //   })} -->
+    // `
+    // // if (!this._entryDef0) {
+    // //   return html`<div style="display: flex; flex: 1; align-items: center; justify-content: center">
+    // //     <mwc-circular-progress indeterminate></mwc-circular-progress>
+    // //   </div>`;
+    // // }
+
+    // // return html`
+    // //   <div style="display: flex; flex-direction: column">
+    // //     <span style="font-size: 18px">EntryDef0</span>
 
 
-    // <title-detail
+    // // <title-detail
 
-    // .value=${this._entryDef0.title}
-    //   style="margin-top: 16px"
-    // ></title-detail>
+    // // .value=${this._entryDef0.title}
+    // //   style="margin-top: 16px"
+    // // ></title-detail>
 
 
-    // <content-detail
+    // // <content-detail
 
-    // .value=${this._entryDef0.content}
-    //   style="margin-top: 16px"
-    // ></content-detail>
+    // // .value=${this._entryDef0.content}
+    // //   style="margin-top: 16px"
+    // // ></content-detail>
 
-    //   </div>
-    // `;
+    // //   </div>
+    // // `;
   }
 
   static get scopedElements() {
